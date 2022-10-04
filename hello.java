eb433383d9191a00ed0d24e5b74521d13c9f969c
rjha27404 <65777075+rjha27404@users.noreply.github.com>
2022-10-04 10:06:10 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 class hello{
   public static void main(String[] args){
-    System.out.printlnss("Hello Rahul Jha");
+    System.out.println("Hello Rahul Jha");
     System.out.println("Hello");
   }
 }
